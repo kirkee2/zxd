76ea85495a1873298fc938fc2bec3e2ad8a6613e
Len <len@Lenui-MacBook-Pro.local>
2017-06-21 09:08:32 +0900

    # Finish

--- app/src/main/java/com/hellmoney/thca/TempAgent.java
+++ app/src/main/java/com/hellmoney/thca/TempAgent.java
@@ -2,5 +2,9 @@ package com.hellmoney.thca;
 
 public class TempAgent {
     // TODO : 로그인 완성되면 대체
-    public final static String AGENT_ID = "agent9@naver.com";
+    public final static String AGENT_ID = "agent1@naver.com";
+//    public final static String AGENT_ID = "agent2@naver.com";
+//    public final static String AGENT_ID = "agent3@naver.com";
+//    public final static String AGENT_ID = "agent4@naver.com";
+//    public final static String AGENT_ID = "agent5@naver.com";
 }
